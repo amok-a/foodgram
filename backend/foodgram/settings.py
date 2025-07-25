595a069dd61f912f35363b310dce02299f554336
Artem Folinger <folingerzlive@gmail.com>
2025-07-25 15:19:44 +0300

    4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=_56kbqzx5-wyjw9#8smwyq2$d%pdh$7_pe!+9&682pa+x6tti'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['51.250.26.157', 'foodgramgram.ddns.net', 'localhost']
 
